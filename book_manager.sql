90bb78d1334dfaca85522a2f228d9b76ffe29e4e
ymkaniaoe <39075882+ymkaniaoe@users.noreply.github.com>
2018-06-07 23:53:19 +0800

    数据库结构更新
    根据管理员这边的使用需要进行了修改

--- book_manager.sql
+++ book_manager.sql
@@ -1,71 +1,27 @@
--- phpMyAdmin SQL Dump
--- version 4.7.4
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: 2018-05-31 10:58:57
--- 服务器版本： 10.1.28-MariaDB
--- PHP Version: 7.1.11
+CREATE DATABASE book_manager;
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `book_manager`
---
-
--- --------------------------------------------------------
-
---
 -- 表的结构 `admin`
---
 
 CREATE TABLE `admin` (
-  `admin_id` int(10) NOT NULL,
+  `admin_id` int(10) PRIMARY KEY AUTO_INCREMENT,
   `admin_name` varchar(15) NOT NULL,
-  `password` varchar(15) NOT NULL
+  `password` varchar(15) NOT NULL,
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
---
--- 表的结构 `admin_record`
---
-
-CREATE TABLE `admin_record` (
-  `id` int(10) NOT NULL,
-  `ISBN` int(20) NOT NULL,
-  `book_name` varchar(50) NOT NULL,
-  `author` varchar(20) NOT NULL,
-  `user_id` int(10) NOT NULL,
-  `user_name` varchar(15) NOT NULL,
-  `apply_type` tinyint(1) NOT NULL,
-  `operation` int(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
 -- 表的结构 `apply`
---
 
 CREATE TABLE `apply` (
-  `id` int(10) NOT NULL,
+  `id` int(10) PRIMARY KEY AUTO_INCREMENT,
   `user_id` int(10) NOT NULL,
-  `apply_type` tinyint(1) NOT NULL,
+  `apply_type` varchar(10) NOT NULL,
   `book_id` int(10) NOT NULL,
   `ISBN` varchar(20) NOT NULL,
-  `approval_state` int(1) NOT NULL DEFAULT '1',
-  `apply_time` int(11) NOT NULL,
-  `reason` text
+  `approval_state` varchar(10) NOT NULL,
+  `apply_time` datetime NOT NULL,
+  `reason` text,
+  `operate_time` datetime DEFAULT NULL,
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -75,13 +31,13 @@ CREATE TABLE `apply` (
 --
 
 CREATE TABLE `book_info` (
-  `book_id` int(10) NOT NULL,
+  `book_id` int(10) PRIMARY KEY AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   `author` varchar(30) NOT NULL,
   `ISBN` varchar(20) NOT NULL,
   `press` varchar(50) NOT NULL,
   `introduction` text NOT NULL,
-  `state` tinyint(1) NOT NULL DEFAULT '1'
+  `state` varchar(10) NOT NULL,
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -91,11 +47,11 @@ CREATE TABLE `book_info` (
 --
 
 CREATE TABLE `reader` (
-  `user_id` int(10) NOT NULL,
+  `user_id` int(10) PRIMARY KEY AUTO_INCREMENT,
   `user_name` varchar(15) NOT NULL,
   `password` varchar(15) NOT NULL,
   `register_day` date NOT NULL,
-  `user_state` tinyint(1) NOT NULL DEFAULT '1'
+  `user_state` tinyint(1) NOT NULL,
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -105,79 +61,14 @@ CREATE TABLE `reader` (
 --
 
 CREATE TABLE `user_record` (
-  `id` int(10) NOT NULL,
+  `id` int(10) PRIMARY KEY AUTO_INCREMENT,
   `user_id` int(10) NOT NULL,
   `ISBN` varchar(20) NOT NULL,
   `book_name` varchar(50) NOT NULL,
   `author` varchar(30) NOT NULL,
   `borrow_time` int(11) NOT NULL,
-  `return_time` int(11) NOT NULL DEFAULT '0'
+  `return_time` int(11) NOT NULL ,
+  FOREIGN KEY (id) REFERENCES reader(user_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Indexes for dumped tables
---
 
---
--- Indexes for table `admin`
---
-ALTER TABLE `admin`
-  ADD PRIMARY KEY (`admin_id`);
-
---
--- Indexes for table `admin_record`
---
-ALTER TABLE `admin_record`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `apply`
---
-ALTER TABLE `apply`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `book_info`
---
-ALTER TABLE `book_info`
-  ADD PRIMARY KEY (`book_id`);
-
---
--- Indexes for table `reader`
---
-ALTER TABLE `reader`
-  ADD PRIMARY KEY (`user_id`);
-
---
--- Indexes for table `user_record`
---
-ALTER TABLE `user_record`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `user_id` (`user_id`);
-
---
--- 在导出的表使用AUTO_INCREMENT
---
-
---
--- 使用表AUTO_INCREMENT `admin_record`
---
-ALTER TABLE `admin_record`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
-
---
--- 使用表AUTO_INCREMENT `apply`
---
-ALTER TABLE `apply`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
-
---
--- 使用表AUTO_INCREMENT `book_info`
---
-ALTER TABLE `book_info`
-  MODIFY `book_id` int(10) NOT NULL AUTO_INCREMENT;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
